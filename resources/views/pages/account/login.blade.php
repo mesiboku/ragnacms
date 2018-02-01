f6d0b5b4ab5c6456454a9ff863bbe0ba3eafb0f9
nasagnilac <official.wedevgames@yahoo.com.ph>
2018-02-01 09:45:40 +0800

    Change registration lang to account.

--- resources/views/pages/account/login.blade.php
+++ resources/views/pages/account/login.blade.php
@@ -13,19 +13,19 @@
         <div class="wrapper-page">
             <div class="login-container">
                 <div class="text-center font-style-1">
-                    {{Lang::get('registration.Login')}}
+                    {{Lang::get('account.Login')}}
                 </div>
                 <form class="form-horizontal m-t-20" action="index.html">
                     <div class="form-group">
                         <div class="col-xs-12">
-                            <input class="form-control" type="text" required="" placeholder="{{Lang::get('registration.Username')}}">
+                            <input class="form-control" type="text" required="" placeholder="{{Lang::get('account.Username')}}">
                             <i class="md md-assignment-ind form-control-feedback l-h-34"></i>
                         </div>
                     </div>
 
                     <div class="form-group">
                         <div class="col-xs-12">
-                            <input class="form-control" type="password" required="" placeholder="{{Lang::get('registration.Password')}}">
+                            <input class="form-control" type="password" required="" placeholder="{{Lang::get('account.Password')}}">
                             <i class="md md-security form-control-feedback l-h-34"></i>
                         </div>
                     </div>
@@ -38,7 +38,7 @@
 
                     <div class="form-group m-t-30">
                         <div class="col-sm-12 text-center">
-                            <a href="/forgotpass" class="text-muted">{{Lang::get('registration.ForgotPass')}}</a>
+                            <a href="/forgotpass" class="text-muted">{{Lang::get('account.ForgotPass')}}</a>
                         </div>
                     </div>
                 </form>
