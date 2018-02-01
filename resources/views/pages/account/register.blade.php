a7f8b3233405a0bd2b2ba4c92354b992df1ce2d4
nasagnilac <official.wedevgames@yahoo.com.ph>
2018-02-01 09:38:18 +0800

    Relocation of files and yield a menu.

--- resources/views/pages/account/register.blade.php
+++ resources/views/pages/account/register.blade.php
@@ -3,6 +3,12 @@
 <!-- Title -->
 @section('pageTitle', Config::get('application.WebsiteName'))
 
+@section('menu')
+<li>
+    <a href="/login">Login</a>
+</li>
+@endsection
+
 @section('content')
         <div class="wrapper-page">
             <div class="reg-container">
